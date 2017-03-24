454f829e8e2fa1e1a7b20431b788657c4f5164ef
Vivian Lin <linvivian7@gmail.com>
2017-03-23 18:01:56 -0700

    Change syntax

--- conver_interval.py
+++ conver_interval.py
@@ -14,34 +14,20 @@ def converge_intervals(a, b):
     for i in range(0, len(a)):
 
         if b[i][0] >= a[i][1] or a[i][0] >= b[i][1]:
-            if b[i][0] >= a[1][1]:
-                lower, upper = a[i]
-            else:
-                lower, upper = b[i]
+            lower, upper = a[i] if b[i][0] >= a[1][1] else b[i]
             result.append((lower, upper))
             break
 
-        if a[i][0] >= b[i][0]:
-            lower = a[i][0]
-        else:
-            lower = b[i][0]
-
-        if a[i][1] <= b[i][1]:
-            upper = a[i][1]
-        else:
-            upper = b[i][1]
+        lower = a[i][0] if a[i][0] >= b[i][0] else b[i][0]
+        upper = a[i][1] if a[i][1] <= b[i][1] else b[i][1]
         result.append((lower, upper))
 
         if i < len(a) - 1:
-            if b[i][1] in range(a[i+1][0], a[i+1][1]+1):
-                lower1 = a[i+1][0]
-                upper1 = b[i][1]
+            if b[i][1] in range(a[i+1][0], a[i+1][1]+1) or a[i][1] in range(b[i+1][0], b[i+1][1]+1):
+                lower1 = a[i+1][0] if b[i][1] in range(a[i+1][0], a[i+1][1]+1) else b[i+1][0]
+                upper1 = b[i][1] if b[i][1] in range(a[i+1][0], a[i+1][1]+1) else a[i][1]
                 result.append((lower1, upper1))
 
-            elif a[i][1] in range(b[i+1][0], b[i+1][1]+1):
-                lower1 = b[i+1][0]
-                upper1 = a[i][1]
-                result.append((lower1, upper1))
     return result
 
 #####################################################################
